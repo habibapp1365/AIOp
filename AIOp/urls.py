40e254d170149de5e5990dd0106306604bed5cb3
habibapp1365 <habib.app1365@gmail.com>
2021-04-17 19:27:10 +0430

    Add login and logout to project

--- AIOp/urls.py
+++ AIOp/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', views.LoginView.as_view(template_name='login.html'), name='login'),          # Class-Based View
+    path('logout/', views.LogoutView.as_view(template_name='logout.html'), name='logout'),      # Class-Based View
     path('', include('RADIUS.urls')),
 ]
