f344f32fdf4a1989400434f57c13c4151d5d9213
habibapp1365 <habib.app1365@gmail.com>
2021-04-20 16:54:41 +0430

    rename Project

--- _AIOp/urls.py
+++ _AIOp/urls.py
@@ -1,4 +1,4 @@
-"""AIOp URL Configuration
+"""_AIOp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
